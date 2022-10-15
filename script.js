c1d532df238d6b4afc73d00a4b139ff763498103
ivsonramos <ivson.ramos@gmail.com>
2022-10-15 16:13:05 -0300

    :construction:Adicionar acões de clique em todos os botões

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ let descrição = document.querySelector('.d-1-4');
 let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
 let numeros = document.querySelector('.d-1-3');
+
+function clicou(n) {
+    alert("clicou em "+n);
+}
+
+function brancon() {
+    alert("clicou em BRANCO");
+}
+
+function corrige() {
+    alert("clicou em CORRIGE");
+}
+
+function confirma() {
+    alert("clicou em CONFIRMA");
+}
