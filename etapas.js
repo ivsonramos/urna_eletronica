d597b9547e6e5ac8c76db30863d6b8a6ec9e031a
ivsonramos <ivson.ramos@gmail.com>
2022-10-14 21:29:53 -0300

    :construction:Adicionar script de candidatos

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'GOVERNADOR',
+        numero: 2,
+        candidato: [
+            {
+                numero: 77,
+                name: 'Marilia Arraes',
+                partido: 'PT',
+                fotos:[
+                    {url:'xxxx.jpg', legenda:'Governador', small:false}
+                ]
+            },
+            {
+            numero: 45,
+                name: 'Raquel Lyra',
+                partido: 'PSDB',
+                fotos:[
+                    {url:'xxxx.jpg', legenda:'Governador', small:false}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 22,
+                name: 'Bolsonaro',
+                partido: 'PL',
+                vice:'Ger. Braga Netto',
+                fotos: [
+                    {url:'xxxx.jpg', legenda:'Presidente'},
+                    {url:'xxx.jpg', legenda: 'Vice-Presidente', small:false}
+                ]
+            },
+            {
+            numero: 13,
+                name: 'Lula',
+                partido: 'PT',
+                vice:'Geraldo Alckmin',
+                fotos:[
+                    {url:'xxxx.jpg', legenda:'Presidente'},
+                    {url:'xxx.jpg', legenda: 'Vice-Presidente', small:false}
+                ]
+            }
+        ]
+    }
+]
